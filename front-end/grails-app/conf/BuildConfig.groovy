5c32b891d2735726155b32104745f859a6c96bb6
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2013-12-02 13:19:09 +0800

    First compile

--- front-end/grails-app/conf/BuildConfig.groovy
+++ front-end/grails-app/conf/BuildConfig.groovy
@@ -58,6 +58,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
+        compile ':db-reverse-engineer:0.5'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
