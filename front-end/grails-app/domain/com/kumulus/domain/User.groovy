4860327326b704d164fa049e54de55345479c494
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2013-12-02 18:01:53 +0800

    Merged scando user class with Spring user class

--- front-end/grails-app/domain/com/kumulus/domain/User.groovy
+++ front-end/grails-app/domain/com/kumulus/domain/User.groovy
@@ -2,6 +2,8 @@ package com.kumulus.domain
 
 class User {
 
+        // Spring definitions
+    
 	transient springSecurityService
 
 	String username
@@ -13,13 +15,48 @@ class User {
 
 	static transients = ['springSecurityService']
 
+        // scando definitions
+        String userId
+	String useridPassword
+	String status
+	String collectionRight
+	String importRight
+	String separationRight
+	Integer importKpiTarget
+	Integer separationKpiTarget
+        Client client
+
+        static hasMany = [attendances: Attendance,
+                        nodeses: Nodes]
+                      
+        static belongsTo = [Client]
+        
+        // mappings and constraints
+    
+    
 	static constraints = {
+                // Spring constraints
 		username blank: false, unique: true
 		password blank: false
+                
+                // ScanDo constraints
+                userId maxSize: 45
+		useridPassword nullable: true, maxSize: 100
+		status nullable: true, maxSize: 10
+		collectionRight nullable: true, maxSize: 1
+		importRight nullable: true, maxSize: 1
+		separationRight nullable: true, maxSize: 1
+		importKpiTarget nullable: true
+		separationKpiTarget nullable: true
 	}
 
 	static mapping = {
+                // Spring mappings
 		password column: '`password`'
+                
+                // Scando mappings
+                id name: "userId", generator: "assigned"
+		version false
 	}
 
 	Set<Role> getAuthorities() {
