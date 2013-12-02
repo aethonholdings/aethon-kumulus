d6fb9d8548cebdd0487f3ae2c5f9de57c2779f52
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2013-12-02 15:34:06 +0800

    Compiles and runs but foreign key errors, likely in node domain class

--- front-end/grails-app/conf/DataSource.groovy
+++ front-end/grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "kumulus"
+    password = "password"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -15,8 +15,8 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/kumulus?autoreconnect=true"
         }
     }
     test {
