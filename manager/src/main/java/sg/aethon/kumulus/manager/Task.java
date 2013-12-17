bb3878641807226a3c04f71790ad2fbffcf9b06a
Theodoros Balopoulos <tbaxx@tbal.info>
2013-12-17 17:56:45 +0100

    Draft implementation of synchronization.
    Needs optimization.

--- manager/src/main/java/sg/aethon/kumulus/manager/Task.java
+++ manager/src/main/java/sg/aethon/kumulus/manager/Task.java
@@ -15,16 +15,22 @@ public interface Task
 {
     enum Status
     {
-        ERROR(-1),
+        ERROR(-1, "ERROR"),
         READY_FOR_UPLOAD(1),
         READY_FOR_BATCH_INSTANCE(2),
         CREATED(3),
-        READY_FOR_REVIEW(4),
-        READY_FOR_VALIDATION(5),
-        FINISHED(6);
+        READY_FOR_REVIEW(4, "READY_FOR_REVIEW"),
+        READY_FOR_VALIDATION(5, "READY_FOR_VALIDATION"),
+        FINISHED(6, "FINISHED");
         
         public final int code;
-        Status(int code) { this.code = code; }
+        public final String eph_name;
+        Status(int code) { this(code, null); }
+        Status(int code, String eph_name)
+        {
+            this.code = code;
+            this.eph_name = eph_name;
+        }
     }
     
     int getPeriod(Properties p);
--- manager/src/main/java/sg/aethon/kumulus/manager/tasks/SynchronizeTask.java
+++ manager/src/main/java/sg/aethon/kumulus/manager/tasks/SynchronizeTask.java
@@ -6,6 +6,12 @@
 
 package sg.aethon.kumulus.manager.tasks;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import sg.aethon.kumulus.manager.Commons;
 import sg.aethon.kumulus.manager.Properties;
 import sg.aethon.kumulus.manager.Task;
 
@@ -15,7 +21,18 @@ import sg.aethon.kumulus.manager.Task;
  */
 public class SynchronizeTask implements Task
 {
-
+    private final Map<String, Status> map = new HashMap<>();
+    
+    public SynchronizeTask()
+    {
+        for (Status status:
+                new Status[] {Status.ERROR, Status.READY_FOR_REVIEW,
+                              Status.READY_FOR_VALIDATION, Status.FINISHED})
+        {
+            map.put(status.eph_name, status);
+        }
+    }
+    
     @Override
     public int getPeriod(Properties p)
     {
@@ -25,7 +42,23 @@ public class SynchronizeTask implements Task
     @Override
     public void execute(Properties p) throws Exception
     {
-        
+        JdbcTemplate kum = Commons.getKumulusConnection(p);
+        List<Integer> ids = kum.queryForList("select batch_instance_id from task "+
+                                             "where status<>? and batch_instance_id is not null",
+                                             Integer.class,
+                                             new Object[] {Status.FINISHED.code});
+        String in_part = StringUtils.join(ids, ",");
+        JdbcTemplate eph = Commons.getEphesoftConnection(p);
+        List<Map<String, Object>> rows =
+            eph.queryForList("select id, batch_status from batch_instance where id in (?)",
+                             new Object[] {in_part});
+        for (Map<String, Object> row: rows)
+        {
+            int id = ((Number) row.get("id")).intValue();
+            Status new_status = map.get(row.get("batch_status").toString());
+            kum.update("update task set status=?, reported=null where batch_instance_id=?",
+                       new Object[] {new_status.code, id});
+        }
     }
     
 }
