8ab704c26d3d2685ad461dc3a4b6643432ba0985
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2014-01-07 23:25:31 +0800

    Changed package name

--- front-end/target/work/gspcompile/gsp_frontEnd_collectworkflow_gsp.groovy
+++ front-end/target/work/gspcompile/gsp_frontEnd_collectworkflow_gsp.groovy
@@ -19,24 +19,22 @@ printHtmlPart(2)
 invokeTag('javascript','g',4,['src':("jstree.min.js")],-1)
 printHtmlPart(3)
 invokeTag('javascript','g',5,['src':("nodeTree.js")],-1)
-printHtmlPart(3)
+printHtmlPart(4)
 createTagBody(2, {->
-createClosureForHtmlPart(4, 3)
-invokeTag('captureTitle','sitemesh',6,[:],3)
+createClosureForHtmlPart(5, 3)
+invokeTag('captureTitle','sitemesh',15,[:],3)
 })
-invokeTag('wrapTitleTag','sitemesh',6,[:],2)
-printHtmlPart(5)
-})
-invokeTag('captureHead','sitemesh',7,[:],1)
-printHtmlPart(5)
-createTagBody(1, {->
+invokeTag('wrapTitleTag','sitemesh',15,[:],2)
 printHtmlPart(6)
-expressionOut.print(project.projectName)
+})
+invokeTag('captureHead','sitemesh',16,[:],1)
+printHtmlPart(6)
+createTagBody(1, {->
 printHtmlPart(7)
-expressionOut.print(nodes)
+expressionOut.print(project.projectName)
 printHtmlPart(8)
 })
-invokeTag('captureBody','sitemesh',17,[:],1)
+invokeTag('captureBody','sitemesh',20,[:],1)
 printHtmlPart(9)
 }
 public static final Map JSP_TAGS = new HashMap()
@@ -44,7 +42,7 @@ protected void init() {
 	this.jspTags = JSP_TAGS
 }
 public static final String CONTENT_TYPE = 'text/html;charset=UTF-8'
-public static final long LAST_MODIFIED = 1388851684000L
+public static final long LAST_MODIFIED = 1389082034000L
 public static final String EXPRESSION_CODEC = 'html'
 public static final String STATIC_CODEC = 'none'
 public static final String OUT_CODEC = 'html'
