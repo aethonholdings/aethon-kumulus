670f3b3da64cd9a1550cee2aa6c7bf178d687226
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2014-01-15 16:52:19 +0800

    Updated the schema

--- database/kumulus-30-12-2013-schema.sql
+++ database/kumulus-30-12-2013-schema.sql
@@ -19,7 +19,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 # Dump of table application_parameter
 # ------------------------------------------------------------
 
@@ -29,7 +28,7 @@ CREATE TABLE `application_parameter` (
   `param_id` int(11) NOT NULL AUTO_INCREMENT,
   `param_name` varchar(50) DEFAULT NULL,
   `param_val` varchar(150) DEFAULT NULL,
-  `project_id` int(11) DEFAULT NULL,
+  `project_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`param_id`),
   KEY `project_id` (`project_id`),
   CONSTRAINT `application_parameter_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
@@ -59,7 +58,7 @@ DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE `attendance` (
   `attendance_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(45) DEFAULT NULL,
-  `project_id` int(11) DEFAULT NULL,
+  `project_id` bigint(20) DEFAULT NULL,
   `login_from_time` datetime DEFAULT NULL,
   `login_to_time` datetime DEFAULT NULL,
   PRIMARY KEY (`attendance_id`),
@@ -74,38 +73,110 @@ CREATE TABLE `attendance` (
 
 
 
+# Dump of table currency
+# ------------------------------------------------------------
+
+CREATE TABLE `currency` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `short_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table document
+# ------------------------------------------------------------
+
+CREATE TABLE `document` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `company` varchar(255) DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `identifier` varchar(30) DEFAULT NULL,
+  `nodes` tinyblob NOT NULL,
+  `thumbnail_id` bigint(20) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK335CD11B6C467322` (`thumbnail_id`),
+  CONSTRAINT `FK335CD11B6C467322` FOREIGN KEY (`thumbnail_id`) REFERENCES `image` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table document_image
+# ------------------------------------------------------------
+
+CREATE TABLE `document_image` (
+  `document_images_id` bigint(20) DEFAULT NULL,
+  `image_id` bigint(20) DEFAULT NULL,
+  KEY `FK711B5D374E4D9053` (`image_id`),
+  KEY `FK711B5D3792AA1420` (`document_images_id`),
+  CONSTRAINT `FK711B5D3792AA1420` FOREIGN KEY (`document_images_id`) REFERENCES `document` (`id`),
+  CONSTRAINT `FK711B5D374E4D9053` FOREIGN KEY (`image_id`) REFERENCES `image` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table image
+# ------------------------------------------------------------
+
+CREATE TABLE `image` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+# Dump of table line_item
+# ------------------------------------------------------------
+
+CREATE TABLE `line_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `amount` float NOT NULL,
+  `currency_id` bigint(20) NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` float DEFAULT NULL,
+  `quantity` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK94ED5C7E172ECD01` (`currency_id`),
+  CONSTRAINT `FK94ED5C7E172ECD01` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 # Dump of table nodes
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `nodes`;
-
 CREATE TABLE `nodes` (
   `node_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `project_id` int(11) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
+  `actual_image_name` longtext,
   `barcode` varchar(45) DEFAULT NULL,
   `comment` varchar(200) DEFAULT NULL,
-  `internal_comment` varchar(200) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `parent_node_id` bigint(20) DEFAULT NULL,
-  `hierarchy` varchar(200) DEFAULT NULL,
-  `thumbnail_image_name` varchar(1000) DEFAULT NULL,
-  `actual_image_name` varchar(1000) DEFAULT NULL,
-  `creator_id` varchar(45) DEFAULT NULL,
   `create_datetime` datetime DEFAULT NULL,
-  `last_update_id` varchar(45) DEFAULT NULL,
-  `last_update_datetime` datetime DEFAULT NULL,
-  `document_sequence_number` int(11) DEFAULT NULL,
+  `creator_id` varchar(255) NOT NULL,
   `creatorldapuid` varchar(45) DEFAULT NULL,
-  `uploaded` bit(1) DEFAULT b'0',
+  `document_sequence_number` int(11) DEFAULT NULL,
+  `hierarchy` varchar(200) DEFAULT NULL,
+  `internal_comment` varchar(200) DEFAULT NULL,
+  `last_update_datetime` datetime DEFAULT NULL,
+  `last_update_id` varchar(255) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `parent_node_id` bigint(20) DEFAULT NULL,
+  `project_id` bigint(20) NOT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `thumbnail_image_name` longtext,
+  `type` varchar(45) DEFAULT NULL,
+  `uploaded` bit(1) DEFAULT NULL,
   PRIMARY KEY (`node_id`),
   KEY `FK64212B14D5E1553` (`project_id`),
   KEY `FK64212B1EFDC50D` (`parent_node_id`),
-  CONSTRAINT `FK64212B14D5E1553` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
-  CONSTRAINT `FK64212B1EFDC50D` FOREIGN KEY (`parent_node_id`) REFERENCES `nodes` (`node_id`)
+  CONSTRAINT `FK64212B1EFDC50D` FOREIGN KEY (`parent_node_id`) REFERENCES `nodes` (`node_id`),
+  CONSTRAINT `FK64212B14D5E1553` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 
 # Dump of table param_names
@@ -119,20 +190,32 @@ CREATE TABLE `param_names` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-
 # Dump of table project
 # ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `project`;
 
 CREATE TABLE `project` (
-  `project_id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `clientldapid` varchar(50) NOT NULL,
   `project_name` varchar(50) DEFAULT NULL,
   `status` varchar(10) DEFAULT NULL,
-  `clientldapid` varchar(50) NOT NULL,
   PRIMARY KEY (`project_id`),
-  UNIQUE KEY `project_name_UNIQUE` (`project_name`)
+  UNIQUE KEY `project_name` (`project_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+# Dump of table project_line_item
+# ------------------------------------------------------------
+
+CREATE TABLE `project_line_item` (
+  `project_line_items_id` bigint(20) DEFAULT NULL,
+  `line_item_id` bigint(20) DEFAULT NULL,
+  KEY `FK7256D823A5491` (`project_line_items_id`),
+  KEY `FK7256D8EAC0028A` (`line_item_id`),
+  CONSTRAINT `FK7256D8EAC0028A` FOREIGN KEY (`line_item_id`) REFERENCES `line_item` (`id`),
+  CONSTRAINT `FK7256D823A5491` FOREIGN KEY (`project_line_items_id`) REFERENCES `project` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 
 
@@ -164,7 +247,7 @@ CREATE TABLE `task` (
   `batch_instance_id` bigint(20) DEFAULT NULL,
   `user_id` varchar(255) NOT NULL,
   `reported` bit(1) NOT NULL DEFAULT b'0',
-  `project_id` int(11) NOT NULL,
+  `project_id` bigint(20) NOT NULL,
   `last_batch_instance_id` bigint(20) DEFAULT NULL,
   `type` varchar(255) NOT NULL,
   `version` bigint(20) NOT NULL,
