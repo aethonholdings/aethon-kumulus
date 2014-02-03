020658fd7358dfb2f45b82b8e4b3427a05d44407
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2014-02-03 16:10:59 +0800

    Ready to change LDAP authentication to test.aethon.sg

--- kumulus/grails-app/conf/BuildConfig.groovy
+++ kumulus/grails-app/conf/BuildConfig.groovy
@@ -66,9 +66,9 @@ grails.project.dependency.resolution = {
         compile ":spring-security-core:2.0-RC2"
         compile ":spring-security-ldap:2.0-RC2"
         compile ":export:1.5"
-        compile ":file-uploader:1.2.1"                  // File upload and download
-//        compile ":tika-parser:1.3.0.1"                // Tika parser
-//        compile ":searchable:0.6.6"                   // Lucene search engine
+        compile ":file-uploader:1.2.1"                // File upload and download
+        compile ":tika-parser:1.3.0.1"                // Tika parser
+        compile ":searchable:0.6.6"                   // Lucene search engine
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
