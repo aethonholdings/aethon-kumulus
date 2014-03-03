a1111304e5014b4cf61012cd30da4ee2e93eea05
sharmakuldeep <skuldeep1989@gmail.com>
2014-03-03 18:00:32 +0530

    task done

--- kumulus/web-app/js/kumulus/process.js
+++ kumulus/web-app/js/kumulus/process.js
@@ -1,4 +1,4 @@
-var pageNo, status = false, rowCounter;
+var pageNo,status=false,rowCounter;
 var currentRowObj;
 
 $(document).ready(function()
@@ -11,11 +11,11 @@ $(document).ready(function()
         select: function(event, ui) {
         }
     });
-
-
-    currentRowObj = $("#lineItems tbody tr");
-
-    datePick($("#lineItems tbody tr").length, $("#lineItemCount").val());
+    
+    
+    currentRowObj= $("#lineItems tbody tr");
+ 
+  datePick($("#lineItems tbody tr").length,$("#lineItemCount").val());
 
 
 
@@ -41,7 +41,7 @@ $(document).ready(function()
         // should just copy the footer here
         var rowCount = $("#lineItems tbody tr").length;
         var status = "";
-
+        
         if (rowCount == 0) {
             addRow();
             $('.new.kumulus-column-page').val(pageNo);
@@ -51,9 +51,9 @@ $(document).ready(function()
         }
         if (status)
         {
-
-            addRow();
-            $('.new.kumulus-column-page').val(pageNo);
+   
+                addRow();
+//            $('.new.kumulus-column-page').val(pageNo);
 //            cloneRow();
         }
 
@@ -63,12 +63,12 @@ $(document).ready(function()
 
     $(document).on('click', '.remove', function() {
         $(this).closest("tr").remove();
-        status = true;
-        rowCounter = $("#lineItems tbody tr").length;
+        status=true;
+        rowCounter= $("#lineItems tbody tr").length;
     });
 
     $(document).on('blur', '.kumulus-column-price', function() {
-
+       
         var tdObjectPrice = $(currentRowObj).find("td .kumulus-column-price");
         var tdObjectQuantity = $(currentRowObj).find("td .kumulus-column-quantity");
         var tdObjectAmount = $(currentRowObj).find("td #amount");
@@ -123,8 +123,8 @@ function cloneRow() {
 function send(obj) {
 
     currentRowObj = obj;
-
-
+    
+   
 }
 
 
@@ -179,8 +179,8 @@ function imagePreview(obj) {
 
 }
 
-function calculateTotalAmount(price) {
-    var total = (parseFloat(price) * parseFloat($(currentRowObj).find("td #quantity").val()));
+function calculateTotalAmount(price){
+    var total=(parseFloat(price)*parseFloat($(currentRowObj).find("td #quantity").val()));
     $(currentRowObj).find("#totalAmount").text(total.toFixed(2));
 
     calculategrandTotalAmount()
@@ -220,7 +220,7 @@ function ConvertFormToJSON(form) {
         $(this).find(":input").each(function(index) {
             subMap[$(this).attr("name")] = $(this).val() || '';
         });
-
+      
         itemList.push(subMap);
     })
 
@@ -269,49 +269,58 @@ function validateLastRow() {
 }
 
 function addRow() {
-
-    var mainCount = $("#lineItemCount").val();
-    var count = $("#lineItems tbody tr").length
-
-    if (status) {
-        if (rowCounter == count) {
-            count = count + count + 3
-        }
-        else {
-            count = count + count
-        }
-    }
-
+       
+     var mainCount=$("#lineItemCount").val();   
+     var count=$("#lineItems tbody tr").length
+   
+    if(status){
+        if(rowCounter==count){
+       count=count+count+3
+       }
+       else{
+        count=count+count   
+       }
+      }
+   
     $("#lineItems tbody").append('<tr onkeypress="send(this)" class="new">' +
             '<td><input id="lineItemId" name="lineItemId" size="4" type="text" value="" class="kumulus-column-id new" readonly></input></td>' +
             '<td><input id="pageNo" name="pageNo" size="2" type="text" value="" class="kumulus-column-page new" onkeypress="CheckNumeric(event)" ></input>' +
             '<input id="pageId" name="pageId" type="hidden" value=""></input></td>' +
             '<td><input id="focus" name="description" size="25" type=text value="" class="kumulus-column-description new" ></input></td>' +
-            '<td><input id="lineItemDate' + mainCount + count + '" name ="lineItemDate" placeholder="Select a date" type="text"  class="pure-input-1"></td>' +
+            '<td><input id="lineItemDate'+mainCount+count+'" name ="lineItemDate" placeholder="Select a date" type="text"  class="pure-input-1"></td>' +
             '<td><input id="quantity" name="quantity" type=text  size="6" value="" class="kumulus-column-quantity new" onkeydown="CheckNumeric(event)"></input></td>' +
             '<td><input id="price" name="price" size="6" type="text" value="" class="kumulus-column-price new" onkeydown="CheckNumeric(event)" onchange="total(this)"></input></td>' +
             '<td><input id="amount" name ="amount" size="6" type="text"  value="" class="kumulus-column-amount new" onkeydown="CheckNumeric(event)" id="test"></input></td>' +
             '<td><a class="remove" href="#" >Remove</a></td>' +
             '</tr>')
-    datePick(count, mainCount);
+    datePick(count,mainCount);
+      $('#lineItems tr:last td #pageNo').val(pageNo);
 }
 
 
-function datePick(counter, mainCounter) {
+function datePick(counter,mainCounter){
 
-    for (var j = 0; j <= mainCounter; j++)
-    {
-        for (var i = 0; i <= counter; i++)
+for(var j=0;j<=mainCounter;j++)
+{
+    for ( var i = 0; i <=counter; i++ ) 
         {
-            $("#lineItemDate" + j + i).datepicker({
-                minDate: new Date(),
+            $("#lineItemDate"+j+i).datepicker({
+            minDate: new Date(),
+              onClose: function () {
+                this.focus();
+                }
             });
+            
         }
     }
-
-    $("#datePickerDate").datepicker({
-        minDate: new Date(),
-    });
+    
+     $("#datePickerDate").datepicker({
+            minDate: new Date(),
+            onClose: function () {
+                this.focus();
+                }
+             
+     });
 
 }
 
