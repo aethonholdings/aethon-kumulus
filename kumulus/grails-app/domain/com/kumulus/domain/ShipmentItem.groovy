972bd50d2f43be79ddf27c85b2b3940650f4eb4a
sharmakuldeep <skuldeep1989@gmail.com>
2014-03-05 11:22:25 +0530

    task done

--- kumulus/grails-app/domain/com/kumulus/domain/ShipmentItem.groovy
+++ kumulus/grails-app/domain/com/kumulus/domain/ShipmentItem.groovy
@@ -5,8 +5,12 @@ class ShipmentItem {
     static final byte TYPE_NODE = 1
     static final byte TYPE_PRODUCT = 2
     
+    static final byte DELIVERY_DROP_OFF = 1
+    static final byte DELIVERY_PICK_UP = 2
+    
     byte type
     long itemId
+    Byte delivery
     Long quantity
     
     static belongsTo = [shipment: Shipment]
