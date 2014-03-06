3bb6078f1daf4b14bf76a7eb765b35b7a41c7f95
Theodoros Balopoulos <tbaxx@tbal.info>
2014-03-06 02:14:27 +0100

    Submit job now uses workflowService

--- kumulus/grails-app/jobs/com/kumulus/jobs/SubmitDocumentJob.groovy
+++ kumulus/grails-app/jobs/com/kumulus/jobs/SubmitDocumentJob.groovy
@@ -10,6 +10,7 @@ import com.abbyy.ocrsdk.Client
 import com.abbyy.ocrsdk.ProcessingSettings
 import com.kumulus.domain.Document
 import com.kumulus.domain.Page
+import com.kumulus.domain.Task
 
 /**
  *
@@ -18,6 +19,7 @@ import com.kumulus.domain.Page
 class SubmitDocumentJob {
 
     def grailsApplication
+    def workflowService
 
     static triggers = {
         simple name: 'Submit Job', startDelay: 0, repeatInterval: 10000  
@@ -36,6 +38,8 @@ class SubmitDocumentJob {
 
         // Submit documents with status STATUS_BUILT to ABBYY
         for (doc in Document.findAll {status == Document.STATUS_BUILT && deleted == false}) {
+            def wtask = Task.find {document == doc && completed == null && type == Task.TYPE_OCR}
+            workflowService.startTask(wtask)
             def task = null
             for (page in Page.findAll {document == doc}) {
                 def filename = page.scanImage.file.path
@@ -45,7 +49,8 @@ class SubmitDocumentJob {
             }
             task = client.processDocument(task.Id, settings)
             doc.ocrTask = task.Id
-            doc.status = Document.STATUS_SUBMITTED
+            workflowService.completeTask(wtask)
+            workflowService.createTask(doc, Task.TYPE_OCR_RETRIEVE, 'kumulus')
             doc.save(flush: true)
         }
     }
