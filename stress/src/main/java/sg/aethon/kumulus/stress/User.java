5f96982a1bb9c664fb9c6997af23ee340839edc5
Theodoros Balopoulos <tbaxx@tbal.info>
2014-04-04 23:00:58 +0200

    Switched to PhantomJS.

--- stress/src/main/java/sg/aethon/kumulus/stress/User.java
+++ stress/src/main/java/sg/aethon/kumulus/stress/User.java
@@ -6,15 +6,13 @@
 
 package sg.aethon.kumulus.stress;
 
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
@@ -101,7 +99,7 @@ public class User implements Runnable {
     {
         try
         {
-            WebDriver driver = new RemoteWebDriver(new URL("http://localhost:9515"), DesiredCapabilities.chrome());
+            WebDriver driver = new PhantomJSDriver();
             String barcode;
             try
             {
