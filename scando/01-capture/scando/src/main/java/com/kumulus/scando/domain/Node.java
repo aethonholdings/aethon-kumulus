42afc89808fa993a5b399d65ef5dd8f1e2e60a8f
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2014-04-15 14:14:14 +0800

    Added POJOs

---
+++ scando/01-capture/scando/src/main/java/com/kumulus/scando/domain/Node.java
@@ -0,0 +1,13 @@
+/*
+ * Encapsulates all properties ScanDo node object
+ * 
+ */
+package com.kumulus.scando.domain;
+
+/**
+ *
+ * @author cocodinos
+ */
+public class Node {
+    
+}
---
+++ scando/01-capture/scando/src/main/java/com/kumulus/scando/domain/Project.java
@@ -0,0 +1,13 @@
+/*
+ * ScanDo project - encapsulates all information for projects held in the database
+ * 
+ */
+package com.kumulus.scando.domain;
+
+/**
+ *
+ * @author cocodinos
+ */
+public class Project {
+    
+}
